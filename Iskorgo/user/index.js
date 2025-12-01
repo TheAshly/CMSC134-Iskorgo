3bcd858490296060111b8915c5c3df53572f20c3
Christian Jave S. Hulleza <hullezachristianjave@gmail.com>
2025-12-02 06:10:19 +0800

    Added user and calendar interface

--- Iskorgo/user/index.js
+++ Iskorgo/user/index.js
@@ -1,3 +1,17 @@
-// Put page specific javascript here
+const loginContainer = document.querySelector(".login-section-container");
+const signupContainer = document.querySelector(".signup-section-container");
+const toLogin = document.querySelector(".to-login-anchor");
+const toSignup = document.querySelector(".to-signup-anchor");
 
 
+toLogin.addEventListener("click", e => {
+    e.preventDefault();
+    loginContainer.classList.remove("hidden");
+    signupContainer.classList.add("hidden");
+});
+
+toSignup.addEventListener("click", e => {
+    e.preventDefault();
+    loginContainer.classList.add("hidden");
+    signupContainer.classList.remove("hidden");
+});
